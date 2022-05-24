85c0c2d22cd09ed8696fa1d22cb7d3effd19963f
Wildhound <wildhound404@gmail.com>
2022-05-24 20:34:54 +0600

    project cards base

---
+++ Components/Organism/ProjectCard.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+
+const Wrapper = styled.div`
+  position: relative;
+  cursor: pointer;
+  max-width: fit-content;
+  &:hover > div {
+    &:first-child {
+      transform: translate(-25px, -200px);
+    }
+    &:last-child {
+      transform: translateY(50px);
+    }
+  }
+  .swiper.swiper-initialized.swiper-horizontal.swiper-pointer-events.swiper-backface-hidden.mySwiper {
+    height: 100%;
+  }
+  .swiper-button-next,
+  .swiper-button-prev {
+    color: white;
+  }
+`;
+const ImageWrapper = styled.div`
+  width: 520px;
+  height: 300px;
+  transition: all 0.5s;
+  position: absolute;
+  bottom: 0;
+  z-index: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: left top;
+`;
+
+const CardContent = styled.div`
+  width: 520px;
+  height: 300px;
+  color: #000;
+  background-color: #31313a;
+  transition: all 0.5s;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-direction: column;
+  flex-direction: column;
+  padding: 0.5em;
+  box-sizing: border-box;
+  color: #fff;
+  padding-top: 3.75rem;
+  > h3 {
+    margin: 0;
+  }
+`;
+
+const ProjectCard = () => {
+  return (
+    <Wrapper>
+      <ImageWrapper>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          <SwiperSlide>
+            <Image src="https://pixicodes.com/static/media/blogCover.2a5d7070.jpeg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="https://pixicodes.com/static/media/blogCover.2a5d7070.jpeg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="https://pixicodes.com/static/media/blogCover.2a5d7070.jpeg" />
+          </SwiperSlide>
+        </Swiper>
+      </ImageWrapper>
+      <CardContent>
+        <h3>Javascript Fundamentals</h3>
+        <p>
+          Some javascript fundamentals can be hard to understand at later
+          stages, Here are some javascript fundamentals explained
+        </p>
+      </CardContent>
+    </Wrapper>
+  );
+};
+
+export default ProjectCard;
--- Components/Sections/PortfolioSection.tsx
+++ Components/Sections/PortfolioSection.tsx
@@ -1,14 +1,47 @@
 import React from "react";
 import styled from "styled-components";
+import ProjectCard from "../Organism/ProjectCard";
 
 const Wrapper = styled.div`
-  background-color: aqua;
+  background-color: #1d1d1d;
   width: 100%;
   height: 100vh;
 `;
 
+const Heading = styled.h1`
+  width: 100%;
+  color: #45f3ff;
+  font-size: 3.25rem;
+  text-align: center;
+  margin-bottom: 0.625rem;
+  font-family: "Koulen", cursive;
+  letter-spacing: 2px;
+  padding-top: 3.5rem;
+  margin-top: 0;
+`;
+
+const ProjectsCardWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  padding: 2.5rem;
+  flex-wrap: wrap;
+  gap: 4rem;
+`;
+
 const PortfolioSection = () => {
-  return <Wrapper>PortfolioSection</Wrapper>;
+  return (
+    <Wrapper>
+      <Heading>Personal Projects</Heading>
+      <ProjectsCardWrapper>
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </ProjectsCardWrapper>
+    </Wrapper>
+  );
 };
 
 export default PortfolioSection;
